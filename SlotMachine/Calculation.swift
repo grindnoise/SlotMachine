fe55a992b86f317002b98efe2a8f5f21b47927fd
Admin <admin@Mac-Admin.local>
2015-04-07 17:05:44 +0300

    first commit

---
+++ SlotMachine/Calculation.swift
@@ -0,0 +1,9 @@
+//
+//  Calculation.swift
+//  SlotMachine
+//
+//  Created by Admin on 06.04.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+import Foundation
---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,9 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Admin on 01.04.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+import Foundation
